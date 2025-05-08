573fd639363e0fa090ceae8dba1ab7e9d4ab6abb
zhanqirui <1550918956@qq.com>
2025-05-08 22:49:09 +0800

    merge rr

--- tests/test1-1.c
+++ tests/test1-1.c
@@ -1,11 +1,9 @@
+// test domain of global var define and local define
+
 int main()
 {
-    int a = 4;
-    int c = 10;
-    int d = a + c;
-    if (a > c) {
-        return 10;
-    }
+    int a = -1;
+    int b = 5;
 
-    return 0;
+    return a + b;
 }
--- tests/test1-1.s
+++ tests/test1-1.s
@@ -1,43 +1,28 @@
 .arch armv8-a
 .text
+.comm a, 4, 4
+.comm b, 4, 4
 .align 1
 .global main
 .type main, %function
 main:
 	stp x16,fp,[sp, #-16]!
-	sub sp,sp,#40
+	sub sp,sp,#20
 	add fp,sp,#0
-	mov x0,#4
+	mov x0,#5
 	str x0,[fp,#8]
-	mov x0,#10
-	str x0,[fp,#16]
 	ldr x0,[fp,#8]
-	ldr x1,[fp,#16]
+	adrp x1,b
+	add x1,x1,:lo12:b
+	ldr x1,[x1]
 	add x2,x0,x1
-	str x2,[fp,#32]
-	ldr x0,[fp,#32]
-	str x0,[fp,#24]
-
-	ldr x0,[fp,#8]
-	ldr x1,[fp,#16]
-	cmp x0,x1
-	cset x2,gt
-	str x2,[fp,#36]
-	ldr x0,[fp,#36]
-	cmp x0,#0
-	b.ne .L1
-	b .L2
-.L1:
-	mov x0,#10
+	str x2,[fp,#16]
+	ldr x0,[fp,#16]
 	str x0,[fp]
-	b .L3
-.L2:
-	mov x0,#0
-	str x0,[fp]
-	b .L3
-.L3:
+	b .L0
+.L0:
 	ldr x0,[fp]
-	add fp,fp,#40
+	add fp,fp,#20
 	mov sp,fp
 	ldp x16,fp,[sp], #16
 	ret
